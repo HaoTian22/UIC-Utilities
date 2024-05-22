b0459975aefcd5c063126ac36cb5b63c80c51180
Hao_Tian22 <Hao_Tian22@outlook.com>
2024-05-22 08:43:01 +0800

    Add feature: Log

--- AutoRestartWIFI.bat
+++ AutoRestartWIFI.bat
@@ -6,10 +6,11 @@ REM You can modify the following settings
 set retry_count=0
 set max_retries=3
 set retry_enabled=1
-set log_file=D:\auto_connect\wifi.log
+set "logFile=logfile.txt"
 set interval=60
 set interval_reattempt=15
 
+echo Starting script > "%logFile%"
 :check_network
 
 REM Some network adapters may have different names, such as "Wi-Fi" or "Wireless Network Connection"
@@ -21,6 +22,7 @@ set "gatewayDetected=false"
 REM ------------------------------------------------------------------------
 REM Do not modify the following lines, unless you know what you are doing
 
+echo Check time: %date% %time% >> "%logFile%"
 REM 使用 netsh 命令检查 WLAN 的状态
 for /f "tokens=*" %%i in ('netsh interface show interface name="%interfaceName%"') do (
     set "line=%%i"
@@ -47,7 +49,7 @@ for /f "tokens=*" %%i in ('ipconfig /all') do (
                 set "wlan_gateway=%%j"
                 REM 清除前导空格
                 set "wlan_gateway=!wlan_gateway: =!"
-                echo WLAN Default Gateway: !wlan_gateway!
+                echo WLAN Default Gateway: !wlan_gateway! >> "%logFile%"
                 set "gatewayFound=false"
                 set "gatewayDetected=true"
             )
@@ -58,31 +60,31 @@ for /f "tokens=*" %%i in ('ipconfig /all') do (
 REM 检查是否找到了默认网关
 REM 因为没连接任何网络的话，我即使重启网卡也是不会自动连上的
 if "!gatewayDetected!"=="false" (
-    echo No default gateway detected for %interfaceName%. The network may not be connected.
+    echo No default gateway detected for %interfaceName%. The network may not be connected. >> "%logFile%"
     timeout /t %interval% /nobreak >nul
     goto :check_network
 )
 
 
-echo Try connecting !wlan_gateway!
+echo Try connecting !wlan_gateway! >> "%logFile%"
 ping !wlan_gateway! -n 1 -w 1000 >nul
 if %errorlevel% neq 0 (
     if !retry_enabled! equ 1 (
         if !retry_count! lss !max_retries! (
             set /a retry_count+=1
-            echo Network connection failed, attempting to reconnect...
+            echo Network connection failed, attempting to reconnect... >> "%logFile%"
             netsh interface set interface "%interfaceName%" admin=disabled
             timeout 2 >nul
             netsh interface set interface "%interfaceName%" admin=enabled
             timeout /t %interval_reattempt% >nul
             goto :check_network
         ) else (
-            echo Maximum attempts reached. Disabling Wi-Fi restart.
+            echo Maximum attempts reached. Disabling Wi-Fi restart. >> "%logFile%"
             set retry_enabled=0
         )
     )
 ) else (
-    echo Internet connected
+    echo Internet connected >> "%logFile%"
     set retry_count=0
     set retry_enabled=1
 )
